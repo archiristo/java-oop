28e02a4ee86bad67240066978e43cc3f899e1c88
iris <109429124+archiristo@users.noreply.github.com>
2025-03-19 11:43:30 +0300

    Add files via upload

---
+++ BigFactorial.java
@@ -0,0 +1,18 @@
+import java.math.*;
+import java.util.Scanner;
+
+public class BigFactorial {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int num = sc.nextInt();
+        System.out.println(num + "! is " + factorial(num));
+    }
+    public static BigInteger factorial(int n) {
+        BigInteger fact = BigInteger.ONE;
+        for (int i = 1; i <= n; i++) {
+            fact = fact.multiply(BigInteger.valueOf(i));
+        }
+        return fact;
+    }
+}
